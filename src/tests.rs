6daae9c5d513a35797bd6b92f338c5a580ba0d4a
Scott Piriou <scott@parity.io>
2020-06-05 13:37:30 +0200

    Fix counter in test

--- src/tests.rs
+++ src/tests.rs
@@ -2,11 +2,11 @@
 #[test]
 fn my_failing_deny_test() {
     let n = 3;
-    let mut _i = 0;
+    let mut i = 0;
     for _ in 0..=n {
         println!("pls");
-        _i += 1;
+        i += 1;
     }
 
-    assert!(n == 4);
+    assert!(i == 6);
 }
